0866f15181dab28b258a9e748ad60cef1c568dd8
olizet <tymon.zietek@gmail.com>
2018-07-03 12:42:52 +0200

    basic config

---
+++ src/main/java/pl/app/config/AppConfig.java
@@ -0,0 +1,54 @@
+package pl.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+@ComponentScan(basePackages = "pl.app")
+@EnableWebMvc
+@EnableTransactionManagement
+//@EnableJpaRepositories(basePackages = "pl.app.repository")
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
+        emfb.setPersistenceUnitName("SkillsToLearnPersistenceUnit");
+        return emfb;
+    }
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager tm = new JpaTransactionManager(emf);
+        return tm;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://localhost:63342");
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        registry.addResourceHandler("/**")
+                .addResourceLocations("/WEB-INF/");
+    }
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+
+    }
+
+}
