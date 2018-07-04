7f5004a53a0fb8b8e7d5c2d3752e0a16721795c7
Tymon  Zietek <31177568+olizet@users.noreply.github.com>
2018-07-04 21:52:30 +0200

    Update Skill.java

--- src/main/java/pl/app/entity/Skill.java
+++ src/main/java/pl/app/entity/Skill.java
@@ -13,7 +13,6 @@ public class Skill {
     @GeneratedValue
     private Long id;
     private String name;
-    private String description;
 
     public Long getId() {
         return id;
@@ -29,13 +28,5 @@ public class Skill {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
